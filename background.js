fb557da908baf5b9c0112642ce223fcf963062c9
YUKI "Piro" Hiroshi <piro.outsider.reflex@gmail.com>
2019-11-21 11:13:10 +0900

    Don't redeclare configs

--- background.js
+++ background.js
@@ -1,11 +1,5 @@
 'use strict';
 
-const configs = new Configs({
-  ignoreTargetBlank:          true,
-  ignoreTargetSelfOrAncestor: true,
-  ignoreTargetNamed:          false
-});
-
 browser.runtime.onMessage.addListener((message, sender) => {
   if (!message ||
       message.type != 'get-options')
