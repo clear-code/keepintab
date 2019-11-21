51e53619e2f8e2c734fc1011572013f6622a4e96
YUKI "Piro" Hiroshi <piro.outsider.reflex@gmail.com>
2019-11-21 10:49:09 +0900

    Apply coding style defined in .eslintrc.js

--- background.js
+++ background.js
@@ -1,19 +1,19 @@
 'use strict';
 
 const configs = new Configs({
-    ignoreTargetBlank:          true,
-    ignoreTargetSelfOrAncestor: true,
-    ignoreTargetNamed:          false
+  ignoreTargetBlank:          true,
+  ignoreTargetSelfOrAncestor: true,
+  ignoreTargetNamed:          false
 });
 
 browser.runtime.onMessage.addListener((message, sender) => {
-    if (!message ||
-        message.type != 'get-options')
-        return;
+  if (!message ||
+      message.type != 'get-options')
+    return;
 
-    return Promise.resolve({
-        ignoreTargetBlank:          configs.ignoreTargetBlank,
-        ignoreTargetSelfOrAncestor: configs.ignoreTargetSelfOrAncestor,
-        ignoreTargetNamed:          configs.ignoreTargetNamed
-    });
+  return Promise.resolve({
+    ignoreTargetBlank:          configs.ignoreTargetBlank,
+    ignoreTargetSelfOrAncestor: configs.ignoreTargetSelfOrAncestor,
+    ignoreTargetNamed:          configs.ignoreTargetNamed
+  });
 });
--- keepintab.js
+++ keepintab.js
@@ -1,48 +1,48 @@
 let options;
 
-const SELF_OR_ANCESTOR = new Set(["_self", "_parent", "_top"]);
+const SELF_OR_ANCESTOR = new Set(['_self', '_parent', '_top']);
 
 function tryRemoveTarget(element)
 {
-    let currentTarget = element.getAttribute("target");
-    if (!currentTarget) {
-        return;
-    }
-    if ((options.ignoreTargetBlank &&
-         currentTarget == "_blank") ||
-        (options.ignoreTargetSelfOrAncestor &&
-         SELF_OR_ANCESTOR.has(currentTarget)) ||
-        options.ignoreTargetNamed) {
-        element.removeAttribute("target");
-    }
+  let currentTarget = element.getAttribute('target');
+  if (!currentTarget) {
+    return;
+  }
+  if ((options.ignoreTargetBlank &&
+       currentTarget == '_blank') ||
+      (options.ignoreTargetSelfOrAncestor &&
+       SELF_OR_ANCESTOR.has(currentTarget)) ||
+      options.ignoreTargetNamed) {
+    element.removeAttribute('target');
+  }
 }
 
 function removeTargetAttributes(parent)
 {
-    Array.from(parent.querySelectorAll("a[target]")).forEach(tryRemoveTarget);
+  Array.from(parent.querySelectorAll('a[target]')).forEach(tryRemoveTarget);
 }
 
 // Create an observer instance
 let observer = new MutationObserver(mutations => {
-    mutations.forEach(m => {
-        if (m.type === "attributes" && m.target.hasAttribute("target")) {
-            tryRemoveTarget(m.target);
-        } else {
-            removeTargetAttributes(m.target);
-        }
-    });
+  mutations.forEach(m => {
+    if (m.type === 'attributes' && m.target.hasAttribute('target')) {
+      tryRemoveTarget(m.target);
+    } else {
+      removeTargetAttributes(m.target);
+    }
+  });
 });
 
 // Pass in the target node, as well as the observer options
 observer.observe(document.body, {
-    childList: true,
-    subtree: true,
-    attributes: true,
-    attributeFilter: ["target"]
+  childList: true,
+  subtree: true,
+  attributes: true,
+  attributeFilter: ['target']
 });
 
 
-browser.runtime.sendMessage({ type: "get-options" }).then(response => {
-    options = response;
-    removeTargetAttributes(document);
+browser.runtime.sendMessage({ type: 'get-options' }).then(response => {
+  options = response;
+  removeTargetAttributes(document);
 });
